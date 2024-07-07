a40c258e1242197d46f8bd8f9267f372744ed2c0
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-07-07 23:33:29 +0530

    Create halfPyramid.java

---
+++ Patterns/halfPyramid.java
@@ -0,0 +1,14 @@
+
+public class halfPyramid {
+    public static void main(String[] args) {
+        
+        for(int line=1;line<=5;line++)
+        {
+            for(int num =1;num<= line ; num++)
+            {
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
+}
