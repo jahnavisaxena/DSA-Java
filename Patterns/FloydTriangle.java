63d1d7842b4e7c6b9adcfa20d8bacd90a1284c08
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-07-08 19:23:48 +0530

    Create FloydTriangle.java

---
+++ Patterns/FloydTriangle.java
@@ -0,0 +1,14 @@
+public class FloydTriangle {
+    public static void main(String[] args) {
+        int counter = 1;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+}
