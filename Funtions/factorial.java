7619336b0985b30ec4e76482e3324e5f6270a1b2
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-07-10 16:48:21 +0530

    Create factorial.java

---
+++ Funtions/factorial.java
@@ -0,0 +1,17 @@
+public class factorial {
+
+    public static int factorial(int n)
+    {
+        int fact = 1;
+        for(int i=1;i<=n;i++)
+        {
+            fact = fact * i;
+        }
+        return fact;
+
+    }
+    public static void main(String[] args) {
+        
+        System.out.println(factorial(4));
+    }
+}
