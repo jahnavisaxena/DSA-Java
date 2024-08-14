ea659e976088f2d2f5f1a81d3bd6a91d40b92fb3
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-08-14 13:19:57 +0530

    Create update.java

---
+++ Array/update.java
@@ -0,0 +1,22 @@
+
+public class update {
+
+    public static void update(int marks[])
+    {
+        for(int i=0;i<marks.length;i++)
+        {
+            marks[i] = marks[i] + 1;
+        }
+    }
+    public static void main(String[] args) {
+        int marks[]= {92,4,11};
+        update(marks);
+        for(int i=0;i<marks.length;i++)
+        {
+            System.out.println(marks[i]+" ");
+        }
+        System.out.println();
+
+    }
+    
+}
