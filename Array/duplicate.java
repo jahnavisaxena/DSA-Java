5fc0299df733c404b459dc99df48d006a845e141
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-08-15 18:46:52 +0530

    Create duplicate.java

---
+++ Array/duplicate.java
@@ -0,0 +1,21 @@
+public class duplicate {
+
+    public static boolean duplicateElement(int num[])
+    {
+        for(int i=0;i<num.length;i++)
+        {
+            for(int j=i+1;j<num.length;j++)
+            {
+                if(num[i] == num[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int num[]={1,2,3,4,1};
+        System.out.println(duplicateElement(num));
+    }
+}
