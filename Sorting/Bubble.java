14771815f5fd56dbe46179a5331282addb16512e
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-08-17 12:40:17 +0530

    Update Bubble.java

--- Sorting/Bubble.java
+++ Sorting/Bubble.java
@@ -1,1 +1,33 @@
 
+public class bubbleSort {
+
+    public static void bubble(int num[])
+    {
+        for(int turn=0;turn<num.length - 1;turn++)
+        {
+            for(int j=0;j<num.length - 1 - turn; j++)
+            {
+                if(num[j] > num[j+1])
+                {
+                    int temp = num[j];
+                    num[j] = num[j+1];
+                    num[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void print(int num[])
+    {
+        for(int i=0;i<num.length;i++)
+        {
+            System.out.print(num[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int num[]={2,5,3,7,8,6};
+        bubble(num);
+        print(num);
+    }
+}
+
