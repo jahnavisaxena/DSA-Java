104c98cd3a19812abb759f01f8f52929e9be3337
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-09-06 23:52:22 +0530

    Create palindrome.java

---
+++ Strings/palindrome.java
@@ -0,0 +1,18 @@
+public class palindrome {
+    public static boolean palindrome(String str)
+    {
+        for(int i=0;i<str.length()/2;i++)
+        {
+            int n=str.length();
+            if(str.charAt(i) != str.charAt(n-1-i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "tanat";
+        System.out.println(palindrome(str));;
+    }
+}
