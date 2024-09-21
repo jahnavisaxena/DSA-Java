730edc8237facb22b768937eb25012b839a0a266
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-09-21 14:01:36 +0530

    Create printdInc.java

---
+++ Recursion/printdInc.java
@@ -0,0 +1,17 @@
+public class inc {
+
+    public static void printInc(int n)
+    {
+        if(n==10)
+        {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printInc(n+1);
+    }
+    public static void main(String[] args) {
+        int n=1;
+        printInc(n);
+    }
+}
