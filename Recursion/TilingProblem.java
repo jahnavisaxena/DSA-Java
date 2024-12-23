44cf6cecadfef0a0411374580f79a7dd5353a7b5
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-12-23 17:29:28 +0530

    Update TilingProblem.java

--- Recursion/TilingProblem.java
+++ Recursion/TilingProblem.java
@@ -1,1 +1,22 @@
+public class TilingProblem {
+    public static int tilingproblem(int n)
+    {
+        //base case
+        if(n==0||n==1)
+        {
+            return 1;
+        }
+        //kaam
+        //vertical choice
+        int vertical = tilingproblem(n-1);
+        //horizontal choice
+        int horizontal = tilingproblem(n-2);
+        //total ways
+        int total_ways= vertical + horizontal;
+        return total_ways;
+    }
+    public static void main(String[] args) {
+        System.out.println(tilingproblem(3));
+    }
+}
 
