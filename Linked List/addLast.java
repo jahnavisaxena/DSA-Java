dc38023c6570c74f1748dc9e8f683beca51079d2
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-07-19 21:07:40 +0530

    Create addLast.java

---
+++ Linked List/addLast.java
@@ -0,0 +1,47 @@
+public class addFirst {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addfirst(int data){//no need to write static as we can use linked list objects    
+        //step1
+        Node newNode = new Node(data);
+        //step 2
+        newNode.next = head;
+        //step3
+        head = newNode;
+
+        if(head == null){
+            newNode = head = tail;
+            return;
+        }
+    }
+    public void addLast(int data){
+        Node newNode =new Node(data);
+
+        tail.next = newNode;
+
+        tail = newNode;
+
+        if(head ==null){
+            newNode = tail = head;
+            return;
+        }
+    }
+
+    public static void main(String[] args) {
+        addFirst ll = new addFirst();
+        ll.addfirst(1);
+        ll.addfirst(2);
+    }
+
+}
+
