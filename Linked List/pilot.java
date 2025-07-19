494b7fcd16bba42143119ce0840ab5a5dd3614f1
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-07-19 13:13:30 +0530

    Create pilot.java

---
+++ Linked List/pilot.java
@@ -0,0 +1,17 @@
+public class pilot{
+    public static void main(String[] args) {
+        Node head = new Node(3);
+        Node tail = new Node(4);
+         
+    }
+}
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data =data ;
+        this.next = null;
+    }
+}
