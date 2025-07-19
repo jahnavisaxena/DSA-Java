d899bcf6082a0b87e28a910c55236ed429c26762
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-07-19 20:41:48 +0530

    Update adddFirst.java

--- Linked List/adddFirst.java
+++ Linked List/adddFirst.java
@@ -11,17 +11,24 @@ public class addFirst {
     public static Node head;
     public static Node tail;
 
-    public void addfirst(int data){//no need to write static as we can use linked list objects
+    public void addfirst(int data){//no need to write static as we can use linked list objects    
         //step1
         Node newNode = new Node(data);
         //step 2
         newNode.next = head;
         //step3
         head = newNode;
+
+        if(head == null){
+            newNode = head = tail;
+            return;
+        }
     }
 
     public static void main(String[] args) {
-        
+        addFirst ll = new addFirst();
+        ll.addfirst(1);
+        ll.addfirst(2);
     }
 
 }
